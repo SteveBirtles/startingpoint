d530522acd7dcdb2bcd43d1897fc5560e60ffb50
Steve Birtles <sbirtles@farnborough.ac.uk>
2021-11-21 20:12:29 +0000

    Add files via upload

---
+++ compile_linux.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -o game game.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17
---
+++ game.cpp
@@ -0,0 +1,86 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+
+class Game : public olc::PixelGameEngine {
+ public:
+  Game() { sAppName = "Insert Game Name Here"; }
+
+ private:
+  float timer = 0;
+  int frames = 0;
+  int fps;
+
+ public:
+  bool OnUserCreate() override {
+    /*
+      Load resources here
+    */
+    return true;
+  }
+
+  bool OnUserUpdate(float fElapsedTime) override {
+    timer += fElapsedTime;
+    frames++;
+    if (timer > 1.0) {
+      fps = frames;
+      frames = 0;
+      timer -= 1;
+    }
+
+    inputs();
+    processes();
+    outputs();
+
+    if (GetKey(olc::Key::ESCAPE).bPressed) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  void inputs() {
+    /*
+      Game controls goes here
+    */
+
+    if (GetKey(olc::Key::SPACE).bHeld) {
+      std::cout << "Space!" << std::endl;
+    }
+    if (GetMouse(0).bHeld) {
+      std::cout << "Click!" << std::endl;
+    }
+  }
+
+  void processes() {
+    /*
+      Game logic goes here
+    */
+  }
+
+  void outputs() {
+    SetPixelMode(olc::Pixel::NORMAL);
+
+    /*
+      Game graphics drawn here
+    */
+
+    if (fps > 0) {
+      auto fpsPosition = olc::vi2d(WINDOW_WIDTH - 70, WINDOW_HEIGHT - 70);
+      DrawStringDecal(fpsPosition, "FPS " + std::to_string(fps));
+    }
+  }
+
+  bool OnUserDestroy() override {
+    std::cout << "Closing game" << std::endl;
+    return true;
+  }
+};
+
+int main() {
+  Game game;
+  if (game.Construct(WINDOW_WIDTH, WINDOW_HEIGHT, 1, 1)) game.Start();
+  return 0;
+}
